9d6a98794c02edce5c98adf316773b736017b372
Dimka <dimka@mail.com>
2020-06-17 22:51:24 +0200

    v 1.4

--- main.go
+++ main.go
@@ -7,11 +7,6 @@ import (
 	"sittme/routing"
 )
 
-const (
-	streamType    = "stream"
-	statusCreated = "created"
-)
-
 var (
 	timeout = flag.Int("timeout", 20, "timeout between interrupted and finished state")
 	port    = flag.String("port", "8086", "server port")
